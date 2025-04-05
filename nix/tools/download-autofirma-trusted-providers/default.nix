6a9f7f5865d423169ce58fea0c70e84d790901e6
pancho horrillo <pancho@pancho.name>
2025-04-05 15:58:28 +0200

    ci(providers): enable bash tracing
    Add xtrace to the default list of bashOptions.

--- nix/tools/download-autofirma-trusted-providers/default.nix
+++ nix/tools/download-autofirma-trusted-providers/default.nix
@@ -7,4 +7,10 @@ writeShellApplication {
   name = "download-autofirma-trusted-providers";
   runtimeInputs = [xmlstarlet jq];
   text = builtins.readFile ./download-autofirma-trusted-providers.sh;
+  bashOptions = [
+    "errexit"
+    "nounset"
+    "pipefail"
+    "xtrace"
+  ];
 }
